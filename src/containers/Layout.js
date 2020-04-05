36dd136103bdb42070fc94e3cfb4b166e75b3369
Debian <admin@ip-172-31-25-48.sa-east-1.compute.internal>
2020-04-05 08:43:23 +0000

    finally got autocomplete working

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core';
-import Autocomplete from  '@material-ui/lab/Autocomplete';
-import axios from 'axios';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -18,28 +16,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-async function get_data(){
-  const response = await axios.get('http://333.isos.tech:8888/api/locations');
-  console.log(response.data);
-  return (response.data);
-}
 
 export default function CustomLayout(props) {
   const classes = useStyles();
-  var data = [];
-  var resdata = get_data();
-  resdata.forEach(data.push)
-
 
   return (
     <div className={classes.root}>
-      <Autocomplete
-        id="combo-box-demo"
-        options={data}
-        getOptionLabel={(option) => option.friendly_name}
-        style={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-      ></Autocomplete>
             {props.children}
     </div>
   );
--- src/containers/LocationListView.js
+++ src/containers/LocationListView.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import LocationList from '../components/LocationList';
-
+import Autocomplete from  '@material-ui/lab/Autocomplete';
+import { TextField } from '@material-ui/core';
 
 class LocationListView extends React.Component {
 
@@ -22,7 +23,16 @@ class LocationListView extends React.Component {
     }
     render() {
         return (
-                <LocationList data={this.state.locations}/>
+            <div>
+            <Autocomplete
+                id="combo-box-demo"
+                options={this.state.locations}
+                getOptionLabel={(option) => option.friendly_name}
+                style={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+            ></Autocomplete>
+            <LocationList data={this.state.locations}/>
+            </div>
         )
     }
 }
