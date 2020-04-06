483d54f683cf0cd958d066f3fed00fccf20bc9e1
Debian <admin@ip-172-31-25-48.sa-east-1.compute.internal>
2020-04-06 22:37:39 +0000

    FINALLY got a plot showing up, learning stuff.

--- src/containers/PlotDetailView.js
+++ src/containers/PlotDetailView.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container'
+
+import Plotly from 'plotly.js';
+
 
 export default class PlotDetail extends React.Component {
 
@@ -10,22 +14,36 @@ export default class PlotDetail extends React.Component {
 
     componentDidMount(){
         const locationFriendlyHash = this.props.match.params.locationFriendlyHash
-        axios.get(`http://172.31.25.48:8888/api/plot/${locationFriendlyHash}confirmed`)
+        axios.get(`http://172.31.25.48:8888/api/series/?friendly_hash=${locationFriendlyHash}&case_type=confirmed`)
         .then(res => {
                       this.setState({plot: res.data});
                       console.log(res.data)
-
+                      Plotly.newPlot('aplot',[
+                        {
+                            type: 'line',
+                            x: this.state.plot.x_axis,
+                            y: this.state.plot.cases,
+                        }
+                    ])
                      }
              )
     }
     
     render() {
+
         return (
             <div>
             <Card style={{ width: '100%' }}>
-                <p>{this.state.plot.name}</p>
+                <Container>
+                    <div id='aplot'></div>
+                </Container>
+                <Container >
+                    <p>{this.state.plot.x_axis}</p>
+                </Container>
+                
             </Card>
             </div>
+            
         )
     }
 }
