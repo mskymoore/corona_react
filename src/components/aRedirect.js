e04f4bb1dba12dbe6b549badc64430a461e23a3e
Debian <admin@ip-172-31-25-48.sa-east-1.compute.internal>
2020-04-07 03:25:01 +0000

    removing some junk

--- src/components/aRedirect.js
+++
@@ -1,7 +0,0 @@
-import { useHistory } from 'react-router-dom';
-
-
-export default function HandleChange(event, value){
-    let history = useHistory();
-    history.push(`/location/${value.friendly_hash}`);
-}
